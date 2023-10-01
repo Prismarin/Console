4c2319e6db81b033f507208526ec5635a63f1a18
Kelvin Komlódi <komlodi.j.kelvin@gmail.com>
2023-10-01 19:53:03 +0200

    resolution settings added

--- src/dev/duuduu/console/backend/DuuDuuEngine.java
+++ src/dev/duuduu/console/backend/DuuDuuEngine.java
@@ -6,17 +6,39 @@ public enum DuuDuuEngine {
 
     DuuDuuEngine() {
         debug = false;
+
+        resolutionWidth = DEFAULT_RESOLUTION_WIDTH;
+        resolutionHeight = DEFAULT_RESOLUTION_HEIGHT;
     }
 
     // - DEBUG ---------------------------------------------------------------------------------------------------------
     private boolean debug;
 
-    void initializeDebug(boolean debug) {
+    final void initializeDebug(boolean debug) {
         this.debug = debug;
     }
 
-    public boolean DEBUG() {
+    public final boolean DEBUG() {
         return debug;
+    }
+
+    // - RESOLUTION ----------------------------------------------------------------------------------------------------
+    private int resolutionWidth;
+    public static final int DEFAULT_RESOLUTION_WIDTH = 1280;
+    private int resolutionHeight;
+    public static final int DEFAULT_RESOLUTION_HEIGHT = 720;
+
+    public final void setResolution(int width, int height) {
+        this.resolutionWidth = width;
+        this.resolutionHeight = height;
+    }
+
+    public final int getResolutionWidth() {
+        return resolutionWidth;
+    }
+
+    public final int getResolutionHeight() {
+        return resolutionHeight;
     }
 
 }
--- src/dev/duuduu/console/backend/EntryPoint.java
+++ src/dev/duuduu/console/backend/EntryPoint.java
@@ -5,11 +5,22 @@ import java.util.Arrays;
 
 public class EntryPoint {
 
+    /**
+     *
+     * @param args DEBUG CONSOLE --RESOLUTION [WIDTH = 1280] [HEIGHT = 720] --TPS [TPS = 60] --THREADS [MODE = multi]
+     */
     public static void main(String[] args) {
         System.out.println("Gathering starting info...");
         ArrayList<String> arguments = new ArrayList<>(Arrays.asList(args));
 
         DuuDuuEngine.ENGINE.initializeDebug(arguments.contains("DEBUG"));
+
+        if (arguments.contains("--RESOLUTION")) {
+            final int resolutionIndex = arguments.indexOf("--RESOLUTION");
+            final int width = Integer.parseInt(arguments.get(resolutionIndex + 1));
+            final int height = Integer.parseInt(arguments.get(resolutionIndex + 2));
+            DuuDuuEngine.ENGINE.setResolution(width, height);
+        }
     }
 
 }
